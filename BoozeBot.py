fc26c3cc10911161064557b9d6e952ea819cb3b8
Connor Mason <connor_a_mason@apple.com>
2015-06-23 11:34:53 -0700

    Saving and loading bottles from file now works

--- BoozeBot.py
+++ BoozeBot.py
@@ -1,7 +1,7 @@
 # Code running on computer (or Raspberry Pi) to control Arduino
 
 # import serial
-import pickle
+import cPickle as pickle
 
 # Open serial connection to Arduino
 # ser = serial.Serial('/dev/ttyACM0', 9600)   # Change device name later ***
@@ -9,14 +9,16 @@ import pickle
 # Data file locations
 bottlesDataFile = "bottleData.pkl"
 
-# Variables/constants
-flOzToMilliliterConversion = 29.5735
+# Constants
+numBottles				   = 3				# Number of bottles BoozeBot holds
+flOzToMilliliterConversion = 29.5735		# Conversion ration from fluid ounces to milliliters
 responseMessage            = "DONE"         # Response message from Arduino telling drink is done
 
 # Bottle class and mapping of bottle name to Bottle class
 bottles = {}
 class Bottle:
-    def __init__(self, name_in, sizeInMilliliters_in, price_in, location_in):
+    def __init__(self, key_in, name_in, sizeInMilliliters_in, price_in, location_in):
+    	self.key			 = key_in
         self.name            = name_in
         self.amountRemaining = sizeInMilliliters_in / flOzToMilliliterConversion
         self.price			 = price_in
@@ -34,21 +36,23 @@ def dispense(liquorName, amountInFlOz):
 
 def saveData():
 	# Save bottle data to file
-	dataFile = open(bottlesDataFile, "wb")
-	pickle.dump(bottles, dataFile)
-	dataFile.close()
+	with open(bottlesDataFile, "wb") as output:
+		for key in bottles:
+			pickle.dump(bottles[key], output, pickle.HIGHEST_PROTOCOL)
 
 def restoreData():
 	# Restore bottle data from file
-	dataFile = open(bottlesDataFile, "rb")
-	bottles = pickle.load(dataFile)
-	dataFile.close()
-
+	with open(bottlesDataFile, "rb") as input:
+		for index in range(0, numBottles):
+			bottleIn = pickle.load(input)
+			bottles[bottleIn.key] = bottleIn
 # /Functions ******************************
 
 
-# bottles["vodka"] = Bottle("Smirnoff", 750, 13.99, 3)
+# bottles["vodka"]	  = Bottle("vodka", "Smirnoff", 750, 13.99, 3)
+# bottles["gin"]   	  = Bottle("gin", "New Amsterdam", 750, 11.99, 5)
+# bottles["triple sec"] = Bottle("triple sec", "Dekuypers", 750, 5.99, 2)
 # saveData()
 
 restoreData()
-print bottles
+print "gin brand: " + bottles["gin"].name
--- test2.py
+++
@@ -1,8 +0,0 @@
-import pickle
-
-d = {}
-dataFile = open(r"data.pkl", "rb")
-d = pickle.load(dataFile)
-dataFile.close()
-
-print d
