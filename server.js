bc42a1898f08f02308827cbaff116e183e3ebce6
Connor Mason <connor@conmason.com>
2015-06-24 08:55:17 -0700

    Added basic relay example code for Johnny Five

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 var Queue = require('firebase-queue');
     Firebase = require('firebase');
 
+var five = require("johnny-five");
+var board = new five.Board();
+
 var queueRef = new Firebase('https://burning-torch-4312.firebaseio.com/queue');
 var queue = new Queue(queueRef, function(data, progress, resolve, reject) {
     console.log(data);
+
+    board.on("ready", function() {
+        var relay = new five.Relay(10);
+
+        // Control the relay in real time
+        // from the REPL by typing commands, eg.
+        //
+        // relay.on();
+        //
+        // relay.off();
+        //
+        this.repl.inject({
+            relay: relay
+        });
+    });
 
     resolve();
 
