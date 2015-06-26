efeb3756f79cecee0e05639984b78df4386d04de
Connor Mason <connor@conmason.com>
2015-06-25 18:19:21 -0700

    Adding recipe now works (no error checking)

--- firebaseFunctions.js
+++ firebaseFunctions.js
@@ -1,7 +1,8 @@
-var ref         = new Firebase('https://boozebot.firebaseio.com');
-var usersRef    = ref.child("Users");
-var recipesRef  = ref.child("Recipes");
-var bottlesRef  = ref.child("Bottles");
+var ref             = new Firebase('https://boozebot.firebaseio.com');
+var usersRef        = ref.child("Users");
+var recipesRef      = ref.child("Recipes");
+var bottlesRef      = ref.child("Bottles");
+var transactionsRef = ref.child("Transactions")
 
 function addUser() {
   var firstnameField = $('#firstname');
@@ -16,13 +17,37 @@ function addUser() {
 }
 
 function addRecipe() {
-  var recipeNameField = $('#recipeName');
+  var recipeNameField  = $('#recipeName');
+  var ingredient1Field = $('#ingredient1');
+  var ingredient2Field = $('#ingredient2');
+  var ingredient3Field = $('#ingredient3');
+  var ingredient4Field = $('#ingredient4');
+  var ingredient5Field = $('#ingredient5');
+  var ingredient6Field = $('#ingredient6');
+  var ingredient7Field = $('#ingredient7');
 
-  recipesRef.push({
-    name: recipeNameField.val()
-  })
+  recipeToAdd = {};
+  recipeData = {
+    ingredient1: ingredient1Field.val(),
+    ingredient2: ingredient2Field.val(),
+    ingredient3: ingredient3Field.val(),
+    ingredient4: ingredient4Field.val(),
+    ingredient5: ingredient5Field.val(),
+    ingredient6: ingredient6Field.val(),
+    ingredient7: ingredient7Field.val()
+  };
+  recipeToAdd[recipeNameField.val()] = recipeData;
+
+  recipesRef.update(recipeToAdd);
 
   recipeNameField.val('');
+  ingredient1Field.val('');
+  ingredient2Field.val('');
+  ingredient3Field.val('');
+  ingredient4Field.val('');
+  ingredient5Field.val('');
+  ingredient6Field.val('');
+  ingredient7Field.val('');
 }
 
 function addBottle() {
@@ -52,3 +77,11 @@ function addBottle() {
   sizeField.val('');
   locField.val('');
 }
+
+// function pourDrink() {
+//   var userPouringDrinkField = $('#userPouringDrink');
+//   var drinkToPourField      = $('#drinkToPourField');
+
+  
+// }
+
