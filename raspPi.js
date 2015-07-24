b8dd72bc73aa51465d399622c5fff7b9a1909b24
Connor Mason <connor_a_mason@apple.com>
2015-07-23 18:53:16 -0700

    Removed Arduino code, added GPIO to raspPi.js

--- raspPi.js
+++ raspPi.js
@@ -1,54 +1,53 @@
 var colors   = require('colors/safe');
-var arduino  = require('duino');
+var Gpio 	 = require('onoff').Gpio;
 var Queue    = require('firebase-queue');
     Firebase = require('firebase');
-    board 	 = new arduino.Board();
+
+// Relay open/closed constants
+var RELAYOPEN 	= 1;
+var RELAYCLOSED = 0;
+
+// Relays for each bottle
+var bottle1  = new Gpio(2,  'out');
+var bottle2  = new Gpio(3,  'out');
+var bottle3  = new Gpio(4,  'out');
+var bottle4  = new Gpio(17, 'out');
+var bottle5  = new Gpio(27, 'out');
+var bottle6  = new Gpio(22, 'out');
+var bottle7  = new Gpio(10, 'out');
+var bottle8  = new Gpio(9,  'out');
+var bottle9  = new Gpio(11, 'out');
+var bottle10 = new Gpio(0,  'out');
+var bottle11 = new Gpio(5,  'out');
+var bottle12 = new Gpio(6,  'out');
+var bottle13 = new Gpio(13, 'out');
+var bottle14 = new Gpio(19, 'out');
+var bottle15 = new Gpio(26, 'out');
+
+// Array for indexing relays
+var relays = [bottle1, bottle2, bottle3, bottle4, bottle5, bottle6, bottle7, bottle8, bottle9, bottle10, bottle11, bottle12, bottle13, bottle14, bottle15];
 
 // Configuration variables
 var verbose 		  = true;
-var timeToPourOneFlOz = 2.2;
-var arduinoPort		  = "/dev/cu.usbmodem14131"
-
-// Create serial port
-var serialPort = new SerialPort(arduinoPort, {
-	baudrate: 9600,
-	parser: serialport.parsers.readline("\n")
-});
+var timeToPourOneFlOz = 2200;
 
 // Will instruct Arduino to pour the liquor
 function pourLiquor(bottleNum, amount) {
     if (verbose) { console.log("  Pouring " + colors.yellow(amount) + "oz of liquor in bottle #" + colors.yellow(bottleNum)); }
 
+    relays[bottleNum - 1].write(RELAYOPEN, setTimeout(function() {
+    	relays[bottleNum - 1].write(RELAYCLOSED);
+    }, amount * timeToPourOneFlOz));
 }
 
-// Wait for Arduino to initialize
-board.on('ready', function() {
-	// Set bottle relay pins
-	board.pinMode(2, 'out');
-	board.pinMode(3, 'out');
-	board.pinMode(4, 'out');
-	board.pinMode(5, 'out');
-	board.pinMode(6, 'out');
-	board.pinMode(7, 'out');
-	board.pinMode(8, 'out');
-	board.pinMode(9, 'out');
-	board.pinMode(10, 'out');
-	board.pinMode(11, 'out');
-	board.pinMode(12, 'out');
-	board.pinMode(13, 'out');
-	board.pinMode(14, 'out');
-	board.pinMode(15, 'out');
-	board.pinMode(16, 'out');
-
-	// Waiting for new drinks sitting in queue and pours them
-	var queueRef = new Firebase('https://boozebot.firebaseio.com/drinkQueue');
-	var queue = new Queue(queueRef, function(data, progress, resolve, reject) {
-	    if (verbose) { console.log(colors.green("BoozeBot making drink: ") + colors.green.underline(data["recipeUsed"])); }
-	    for (var attributeName in data) {
-	        if (attributeName.substring(0, 10) == "ingredient") {
-	            pourLiquor(data[attributeName]["liquorBottleNum"], data[attributeName]["amountUsed"]);
-	        }
-	    }
-	    resolve();
-	});
+// Waiting for new drinks sitting in queue and pours them
+var queueRef = new Firebase('https://boozebot.firebaseio.com/drinkQueue');
+var queue = new Queue(queueRef, function(data, progress, resolve, reject) {
+    if (verbose) { console.log(colors.green("BoozeBot making drink: ") + colors.green.underline(data["recipeUsed"])); }
+    for (var attributeName in data) {
+        if (attributeName.substring(0, 10) == "ingredient") {
+            pourLiquor(data[attributeName]["liquorBottleNum"], data[attributeName]["amountUsed"]);
+        }
+    }
+    resolve();
 });
