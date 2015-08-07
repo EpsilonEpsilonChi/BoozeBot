71c718b41c33215d9d6ebb2e7cd0f5932075113f
Connor Mason <connor_a_mason@apple.com>
2015-08-07 10:14:36 -0700

    Added collapsable ingredient detail to transactions summary output

--- helpers/analyzeData.py
+++ helpers/analyzeData.py
@@ -18,13 +18,31 @@ workbook = xlsxwriter.Workbook(transactionDataPath + outputFileName)
 
 # Define formats
 headerFormat    = workbook.add_format({'align': 'center', 'bold': True, 'underline': True})
-timestampFormat = workbook.add_format({'num_format': 'mm/dd/yyyy hh:mm:ss AM/PM'})
-numDrinksFormat = workbook.add_format({'align': 'center'})
+subHeaderFormat = workbook.add_format({'align': 'center', 'underline': True})
+timestampFormat = workbook.add_format({'bold': True, 'num_format': 'mm/dd/yyyy hh:mm:ss AM/PM'})
+drinkFormat 	= workbook.add_format({'bold': True, 'align': 'left'})
+numDrinksFormat = workbook.add_format({'bold': True, 'align': 'center'})
+totalCostFormat = workbook.add_format({'bold': True, 'align': 'center'})
+lineItemFormat  = workbook.add_format({'align': 'right', 'underline': True})
+amountFormat	= workbook.add_format({'align': 'center'})
 costFormat		= workbook.add_format({'align': 'center'})
 
 # Specify number formats
 numDrinksFormat.set_num_format('0.0')
+totalCostFormat.set_num_format(0x07)
+amountFormat.set_num_format('0.0')
 costFormat.set_num_format(0x07)
+
+# Add borders to formats
+borderStyle = 1
+timestampFormat.set_top(borderStyle)
+drinkFormat.set_top(borderStyle)
+numDrinksFormat.set_top(borderStyle)
+totalCostFormat.set_top(borderStyle)
+timestampFormat.set_bottom(borderStyle)
+drinkFormat.set_bottom(borderStyle)
+numDrinksFormat.set_bottom(borderStyle)
+totalCostFormat.set_bottom(borderStyle)
 
 # Returns true if inputted value is a file that should be ignored, false otherwise
 def shouldFileBeIgnored(fileIn):
@@ -79,12 +97,37 @@ for user in os.listdir(mostRecentPath):
 		for key, value in userData.iteritems():
 			timestamp = datetime.datetime.strptime(value["timestamp"], "%m-%d-%Y %H:%M:%S")
 
+			# Write basic information
 			curUserSheet.write_datetime("A" + str(counter), timestamp, timestampFormat)
-			curUserSheet.write("B" + str(counter), value["recipeUsed"])
+			curUserSheet.write("B" + str(counter), value["recipeUsed"], drinkFormat)
 			curUserSheet.write_number("C" + str(counter), value["numStandardDrinks"], numDrinksFormat)
-			curUserSheet.write_number("D" + str(counter), value["totalCost"], costFormat)
+			curUserSheet.write_number("D" + str(counter), value["totalCost"], totalCostFormat)
 
 			counter += 1
+			beginCollapsedRange = counter
+
+			# Write ingredient information
+			curUserSheet.write("B" + str(counter), "Ingredient Name", subHeaderFormat)
+			curUserSheet.write("C" + str(counter), "Amount (fl oz)", subHeaderFormat)
+			curUserSheet.write("D" + str(counter), "Item Price", subHeaderFormat)
+			curUserSheet.set_row(counter - 1, None, None, {'level': 1, 'hidden': True})
+
+			counter += 1
+
+			ingredientNum = 1
+			for x in range(0, value["ingredientCount"]):
+				curIngredient = value["ingredient" + str(ingredientNum)]
+
+				curUserSheet.write("A" + str(counter), "Ingredient " + str(ingredientNum) + ":", lineItemFormat)
+				curUserSheet.write("B" + str(counter), curIngredient["liquorName"])
+				curUserSheet.write("C" + str(counter), curIngredient["amountUsed"], amountFormat)
+				curUserSheet.write("D" + str(counter), curIngredient["lineItemPrice"], costFormat)
+				curUserSheet.set_row(counter - 1, None, None, {'level': 1, 'hidden': True})
+
+				ingredientNum += 1
+				counter += 1
+
+			curUserSheet.set_row(counter - 2, None, None, {'level': 1, 'hidden': True, 'collapsed': True})
 
 # Close workbook
 workbook.close()
