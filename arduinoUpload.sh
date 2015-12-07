20e2eb7e34a4a56819e247b6175218103fed5a7a
Connor Mason <connor@conmason.com>
2015-12-07 14:29:09 -0500

    arduino upload/setup script fixes

--- arduinoUpload.sh
+++ arduinoUpload.sh
@@ -1,18 +1,32 @@
 #!/bin/bash
 
-# Compiles and uploads arduino/arduino.ino
-# Only works on Mac, not Rasp Pi 
+# Compiles and uploads arduinoCode/src/sketch.ino
+
+# **Requires piSetup.sh be run first for Raspberry Pi**
+# **Requires Arduino IDE be installed for Mac**
 
 function echo_bright_green {
     echo -e "\033[1;32m"$@"\033[m"
 }
 
-APP_PATH="/Applications/Arduino.app/Contents/MacOS/Arduino"
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-SKETCH_PATH=$DIR/arduino/arduino.ino
+arch=$(uname -m)
+if [ "$arch" == "x86_64" ]
+then
+	echo "Running on computer"
 
-echo_bright_green "Compiling and uploading arduino/arduino.ino"
-$APP_PATH --upload $SKETCH_PATH
-echo_bright_green "Uploaded"
+	APP_PATH="/Applications/Arduino.app/Contents/MacOS/Arduino"
+	DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+	SKETCH_PATH=$DIR/arduino/arduino.ino
 
+	echo_bright_green "Compiling and uploading arduino/arduino.ino"
+	$APP_PATH --upload $SKETCH_PATH
+	echo_bright_green "Uploaded"
+elif [ "$arch" == "armv6l" ]
+then
+	echo "Running on ARM (Raspberry Pi)"
 
+	cd arduinoCode
+	ino build
+	ino upload
+	cd ..
+fi
---
+++ piSetup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# NOT YET COMPLETE
+# Runs commands to setup environment once the repo has been pulled
+
+function echo_bright_green {
+    echo -e "\033[1;32m"$@"\033[m"
+}
+
+# Setup tools for compiling/uploading Arduino on Pi
+echo_bright_green "Beginning installation..."
+sudo apt-get update
+echo_bright_green "Installing Arduino package..."
+sudo apt-get install arduino
+echo_bright_green "Installing Python dev/setup tools..."
+sudo apt-get install python-dev&&python-setuptools
+echo_bright_green "Cloning inotool repo (handy-dandy Arduino command-line tools)..."
+git clone git://github.com/amperka/ino.git
+echo_bright_green "Installing inotool..."
+cd ino
+sudo python setup.py install
+echo_bright_green "Installing picocom to make ino's serial terminal functional..."
+sudo apt-get install picocom
+cd ..
+
