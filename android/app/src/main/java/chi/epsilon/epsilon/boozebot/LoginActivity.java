941168be201e86e26b062f899a304b561e0de49f
kenzshelley <kenzshelley@gmail.com>
2015-12-31 18:52:15 -0500

    Actually display fragment

--- android/app/src/main/java/chi/epsilon/epsilon/boozebot/LoginActivity.java
+++ android/app/src/main/java/chi/epsilon/epsilon/boozebot/LoginActivity.java
@@ -1,5 +1,7 @@
 package chi.epsilon.epsilon.boozebot;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,15 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = new LoginFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
     }
 }
