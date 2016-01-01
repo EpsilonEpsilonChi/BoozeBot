e67e2801297b4dc0c0cc0de1917eea54ce60c3e2
kenzshelley <kenzshelley@gmail.com>
2016-01-01 12:45:46 -0500

    Set up Firebase auth stuff

--- android/README.md
+++ android/README.md
@@ -3,3 +3,7 @@ Android app for BoozeBot
 
 ## Setup Notes
 1) Ensure that Firebase is enabled in Android Studio by going to: 'File->Project Structure->Cloud' and checking 'Firebase'. 
+
+## TODO
+* get rid of weird red lines around EditTexts on login page
+* figure out why app title bar isn't going away on actual device (it's gone on layout previews)
--- android/app/src/main/java/chi/epsilon/epsilon/boozebot/LoginActivity.java
+++ android/app/src/main/java/chi/epsilon/epsilon/boozebot/LoginActivity.java
@@ -5,11 +5,20 @@ import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.firebase.client.Firebase;
+
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Firebase.setAndroidContext(this);
+//        Firebase rootRef = new Firebase("https://boozebot.firebaseio.com/");
+//        Firebase rootRef = new Firebase("https://kenzandroidtest.firebaseio.com/");
+//        rootRef.child("users").push().setValue("Kenziscool");
+//        rootRef.child("users").child("newuser").setValue("new one");
+
+
         setContentView(R.layout.activity_login);
 
         FragmentManager fm = getSupportFragmentManager();
