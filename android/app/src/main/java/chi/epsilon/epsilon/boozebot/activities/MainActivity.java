f4207b1998fbf9695b08b28a5ea8a4d37dc1e203
kenzshelley <kenzshelley@gmail.com>
2016-01-01 17:28:45 -0500

    main activity files

---
+++ android/app/src/main/java/chi/epsilon/epsilon/boozebot/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package chi.epsilon.epsilon.boozebot.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import chi.epsilon.epsilon.boozebot.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
