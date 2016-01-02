2e4a5f934ffc9d815f992f63084e68bdbeabfa50
kenzshelley <kenzshelley@gmail.com>
2016-01-02 11:50:51 -0500

    attempting to deploy

--- api/app.js
+++ api/app.js
@@ -6,10 +6,17 @@ app.get('/', function(req, res) {
     res.send('hello world');
 });
 
-var server = app.listen(3000, function () {
+//var server = app.listen(3000, function () {
 //  var host = server.address().address;
-  var host = '127.0.0.1'
-  var port = server.address().port;
+ // var host = '127.0.0.1'
+ // var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  //console.log('Example app listening at http://%s:%s', host, port);
+//});
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+app.listen(server_port, server_ip_address, function () {
+    console.log( "Listening on " + server_ip_address + ", server_port " + port )
 });
