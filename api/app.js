533c22408eb9b8f1ce43b202deaaab03d94cb486
kenzshelley <kenzshelley@gmail.com>
2016-01-02 12:06:37 -0500

    updated:

--- api/app.js
+++ api/app.js
@@ -18,5 +18,5 @@ var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
  
 app.listen(server_port, server_ip_address, function () {
-    console.log( "Listening on " + server_ip_address + ", server_port " + port )
+    console.log( "Listening on " + server_ip_address + ", server_port " + server_port )
 });
